cc321ec92dd43b54745ecd78980ca3b7f62dea7a
arifurrahman01 <arifurrahman.now@gmail.com>
2022-09-14 22:52:18 +0600

    start button issue resolved

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -110,7 +110,7 @@ const start = () => {
     countdownOverlay.innerHTML = '<h1>${count}</h1>';
 
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
